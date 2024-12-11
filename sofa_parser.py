3a34fc38bd7db32ce26c2962b1ca613656022fa5
indukumar <v.h.indukumar@gmail.com>
2024-12-11 22:02:17 +0100

    Initial commit

---
+++ sofa_parser.py
@@ -0,0 +1,20 @@
+from lark import Lark, Transformer
+from lark.indenter import Indenter, PythonIndenter
+
+
+class SofaIndenter(Indenter):
+    NL_type = '_NL'
+    OPEN_PAREN_types = [] #['LPAR', 'LSQB', 'LBRACE']
+    CLOSE_PAREN_types = [] #['RPAR', 'RSQB', 'RBRACE']
+    INDENT_type = '_INDENT'
+    DEDENT_type = '_DEDENT'
+    tab_len = 4
+
+
+with open("sofa.lark") as f:
+    lark_parser = Lark(f.read(), parser='lalr', transformer=Transformer(), postlex=SofaIndenter())
+    with open("test/resources/full_scope.sofa") as sa:
+        print(lark_parser.parse(sa.read()).pretty())
+
+    with open("test/resources/simple.sofa") as sa:
+        print(lark_parser.parse(sa.read()).pretty())
