41af29f9067f051617087d1de3c959d89e7e6ae5
indukumar <v.h.indukumar@gmail.com>
2025-01-23 20:30:35 +0100

    Some parsing tests

--- tests/parser/sofa_parser_test.py
+++ tests/parser/sofa_parser_test.py
@@ -1,4 +1,8 @@
 import pytest
+import os.path
+import lark
+from textwrap import dedent
+
 from sofaman.parser.sofa_parser import SofaParser
 
 class TestSofaParser:
@@ -7,16 +11,38 @@ class TestSofaParser:
     def parser(self):
         return SofaParser()
 
-    def test_parse_valid_input(self, parser):
-        content = """
-        valid input content here
-        """
+    def test_parse_all_valid_input(self, parser):
+        dir = os.path.dirname(os.path.realpath(__file__))
+        full_all = os.path.join(dir, 'full_all.sofa')
+        with open(full_all) as f:
+            content = f.read()
         result = parser.parse(content)
-        assert result is not None
+        assert isinstance(result, lark.tree.Tree)
 
     def test_parse_invalid_input(self, parser):
         content = """
-        invalid input content here
+            Random
         """
         with pytest.raises(Exception):
             parser.parse(content)
+    
+    def test_parse_package_variations(self, parser):
+        content = """
+                    package A.B:
+                        visibility: public
+                    package C
+        """
+        result = parser.parse(dedent(content))
+        assert isinstance(result, lark.tree.Tree)
+
+    def test_parse_diagram_variations(self, parser):
+        content = """
+                    diagrams:
+                        - Overview
+                        - "CRM Ecosystem"
+                        - "Technical Architecture":
+                            type: component
+                    diagrams: [A, B, C]
+        """
+        result = parser.parse(dedent(content))
+        assert isinstance(result, lark.tree.Tree)
